64c00856251b05a51702bc0719b9f12647d97091
Rifai19 <Riffaai19@gmail.com>
2016-09-21 21:36:02 +0700

    array 2D soal

--- app/src/main/java/ayyash/app/seamolec_udj/Soal.java
+++ app/src/main/java/ayyash/app/seamolec_udj/Soal.java
@@ -14,11 +14,6 @@ public class Soal extends AppCompatActivity {
     TextView tv;
     int id_quiz;
 
-
-    String opsiSoal[] = { "Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D",
-            "Pilihan d" };
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,31 +23,42 @@ public class Soal extends AppCompatActivity {
          id_quiz = i.getIntExtra("kirimanIDQuiz", 0);
         tv.setText(""+id_quiz);
 
-
         LinearLayout mLinearLayout = (LinearLayout) findViewById(R.id.mLinearLayout);
 
+        String [] Soal = {"Berapa?", "Siapa?", "Kapan?", "Dimana?"};
+        String[][] opsiSoal={
+                {"1", "2", "3"} ,
+                { "saya", "kamu", "dia", "mereka", "kita"},
+                {"sekarang", "waktu itu"},
+                {"disini", "disana"}
+        } ;
 
-        for (int k = 1; k <= 20; k++) {
+//        int jml_soal = opsiSoal.length - 1;
+//        for (int x =0; x<Soal.length; x++){
+//            System.out.println("soal "+ Soal[x]);
+//            for (int y=0; y < opsiSoal[x].length; y++){
+//                System.out.println(opsiSoal[x][y]);
+//            }
+//        }
+
+        for (int k = 0; k < Soal.length; k++) {
             //create text button
+
             TextView tempatSoal = new TextView(this);
-            tempatSoal.setText("Question Number:" + k);
+            tempatSoal.setText(Soal[k]);
             tempatSoal.setTextColor(Color.BLUE);
             mLinearLayout.addView(tempatSoal);
             // create radio button
-            final RadioButton[] rb = new RadioButton[5];
+            final RadioButton[] rb = new RadioButton[6];
             RadioGroup rg = new RadioGroup(this);
             rg.setOrientation(RadioGroup.VERTICAL);
-            for (int i1= 0; i1 < 5; i1++) {
-                rb[i1] = new RadioButton(this);
-                rg.addView(rb[i1]);
-                rb[i1].setText(opsiSoal[i1]);
+                for (int l=0; l<opsiSoal[k].length; l++ ){
+                    rb[l] = new RadioButton(this);
+                    rg.addView(rb[l]);
+                    rb[l].setText(opsiSoal[k][l]);
+                }
 
-            }
             mLinearLayout.addView(rg);
         }
-
-
-
-
     }
 }
